90de9551482539834468cec969f467ffecbb4dfa
lampt1981 <lamtrongphan@gmail.com>
2014-12-27 16:17:29 +0700

    update model standard

--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/rest/resources/NewsServerResource.java
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/rest/resources/NewsServerResource.java
@@ -1,17 +1,24 @@
 package net.dontdrinkandroot.example.angularrestspringsecurity.rest.resources;
 
 import com.joyaether.datastore.resource.ReadonlyDataObjectServerResource;
+
 import net.dontdrinkandroot.example.angularrestspringsecurity.entity.News;
 
 /**
  * Created by HIP on 24/12/2014.
  */
-public class NewsServerResource extends ReadonlyDataObjectServerResource<News> {
+public class NewsServerResource extends ReadonlyDataObjectServerResource<News, Long> {
 
-    @Override
-    protected ID getIdentifier(T model) {
-        return super.getIdentifier(model);
-    }
+   @Override
+protected Long getId() {
+	// TODO Auto-generated method stub
+	return null;
+}
 
+   @Override
+	protected Class<News> getResourceType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
     
 }
